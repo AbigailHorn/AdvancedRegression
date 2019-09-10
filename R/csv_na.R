0a2447830ca9dec95e0bbc1a3f9edca8cedbf712
John Peach <githubspam@jppeach.com>
2019-09-09 21:47:35 -0700

    relabel everything to 'exercise'

--- R/csv_na.R
+++
@@ -1,8 +0,0 @@
-# Get the 'NA' for reading CSV files
-get_csv_nas <- function(chapter, exercise) {
-  if (chapter == 9 & exercise == 6) {
-    c(".", "", "NA")
-  } else {
-    c("", "NA")
-  }
-}
---
+++ R/csv_na_exercise.R
@@ -0,0 +1,8 @@
+# Get the 'NA' for reading CSV files
+get_csv_nas_exercise <- function(chapter, exercise) {
+  if (chapter == 9 & exercise == 6) {
+    c(".", "", "NA")
+  } else {
+    c("", "NA")
+  }
+}
